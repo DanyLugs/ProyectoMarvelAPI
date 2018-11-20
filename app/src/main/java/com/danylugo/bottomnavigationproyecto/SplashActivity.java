96fa3be11cb9d24878cd284821c9d10e4fa49619
Fernando Millán <39173770+FernandoMillan@users.noreply.github.com>
2018-11-20 13:09:54 -0600

    Splash Activity

---
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.danylugo.bottomnavigationproyecto;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
