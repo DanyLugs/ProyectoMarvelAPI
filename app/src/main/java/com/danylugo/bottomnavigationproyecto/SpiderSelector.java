6589fb1dac630bbb21beb3c0564615041ec177ab
RamonArenas1 <ramon_cmk@ciencias.unam.mx>
2018-11-20 23:34:05 -0600

    Card

--- app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 
 public class SpiderSelector extends AppCompatActivity {
 
-    CardView card = (CardView) findViewById(R.id.card);
+    Button card = (Button) findViewById(R.id.card);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +17,16 @@ public class SpiderSelector extends AppCompatActivity {
         setContentView(R.layout.activity_spider_selector);
 
         getSupportActionBar().hide();
+
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MainActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
 
 
 }
