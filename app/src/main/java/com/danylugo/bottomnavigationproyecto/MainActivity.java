51a062c9b789d63bdadcab3701c559716a9db2a2
Daniel Lugo <danylugo@ciencias.unam.mx>
2018-11-20 18:27:25 -0600

    bottom nav

---
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/MainActivity.java
@@ -0,0 +1,82 @@
+package com.danylugo.bottomnavigationproyecto;
+
+import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.danylugo.bottomnavigationproyecto.Fragments.AliadosFragment;
+import com.danylugo.bottomnavigationproyecto.Fragments.PersonajeFragment;
+import com.danylugo.bottomnavigationproyecto.Fragments.VillanosFragment;
+
+import java.util.Objects;
+
+import static com.danylugo.bottomnavigationproyecto.R.color.colorPrimary;
+
+public class MainActivity extends AppCompatActivity {
+
+    public BottomNavigationView mMain_nav;
+    public FrameLayout mMain_frame;
+
+    private AliadosFragment aliadosFragment;
+    private PersonajeFragment personajeFragment;
+    private VillanosFragment villanosFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        mMain_frame = (FrameLayout) findViewById(R.id.main_frame);
+        mMain_nav = (BottomNavigationView) findViewById(R.id.btmNavigationView);
+
+        personajeFragment = new PersonajeFragment();
+        aliadosFragment = new AliadosFragment();
+        villanosFragment = new VillanosFragment();
+
+        mMain_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.itemPersonaje:
+                        mMain_nav.setItemBackgroundResource(R.color.colorPersonaje);
+                        setFragment(personajeFragment);
+                        return true;
+
+                    case R.id.itemAlly:
+                        mMain_nav.setItemBackgroundResource(R.color.colorAliados);
+                        setFragment(aliadosFragment);
+                        return true;
+
+                    case R.id.itemVillain:
+                        mMain_nav.setItemBackgroundResource(R.color.colorVillano);
+                        setFragment(villanosFragment);
+                        return true;
+
+
+                        default:
+                            return false;
+
+                }
+                //return true;
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
