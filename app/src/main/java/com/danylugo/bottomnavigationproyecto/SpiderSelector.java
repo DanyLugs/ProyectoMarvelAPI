4f41db29422e94e8a7a8189385e68d94d498a5d7
RamonArenas1 <ramon_cmk@ciencias.unam.mx>
2018-11-21 01:24:08 -0600

    Activity con Card mejorada

--- app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
@@ -1,6 +1,7 @@
 package com.danylugo.bottomnavigationproyecto;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -9,7 +10,7 @@ import android.widget.Button;
 
 public class SpiderSelector extends AppCompatActivity {
 
-    Button card = (Button) findViewById(R.id.card);
+    //Button card = (Button) findViewById(R.id.card);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +18,15 @@ public class SpiderSelector extends AppCompatActivity {
         setContentView(R.layout.activity_spider_selector);
 
         getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        card.setOnClickListener(new View.OnClickListener() {
+        /**card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (v.getContext(), MainActivity.class);
                 startActivityForResult(intent, 0);
             }
-        });
+        });*/
     }
 
 
