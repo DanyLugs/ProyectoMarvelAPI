5400444d8b1de53ee163d9396e0f07b6b7e0ea83
Fernando Millán <39173770+FernandoMillan@users.noreply.github.com>
2018-11-22 13:36:57 -0600

    API

---
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/MarvrelAPI/Constants.java
@@ -0,0 +1,11 @@
+package com.danylugo.bottomnavigationproyecto.MarvrelAPI;
+
+public class Constants {
+    public static final String VERSION = "/v1/";
+    public static final String REST_ENDPOINT = "public/character";
+    public static final String ROOT_URL = "http://gateway.marvel.com"+VERSION;
+
+    public static final String APIKEY = "004fb341a8b24627ab38a741ac6c9dd2";
+    public static final String TS = "1";
+    public static final String HASH = "004fb341a8b24627ab38a741ac6c9dd2";
+}
