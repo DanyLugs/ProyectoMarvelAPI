191d7a55bcc4cbe7f2dfc295d5f1967ed8908692
RamonArenas1 <ramon_cmk@ciencias.unam.mx>
2018-11-21 23:54:48 -0600

    Spider selector después del splash

--- app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
@@ -1,11 +1,15 @@
 package com.danylugo.bottomnavigationproyecto;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class SpiderSelector extends AppCompatActivity {
 
+    private CardView card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,8 +17,17 @@ public class SpiderSelector extends AppCompatActivity {
 
         getSupportActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        card = (CardView) findViewById(R.id.card);
     }
 
-
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.card:
+                Intent intent = new Intent(SpiderSelector.this, MainActivity.class);
+                startActivity(intent);
+            break;
+        }
+    }
 
 }
