e03340373a4c2c1eccb170c64ed29af4290ddddb
Fernando Millán <39173770+FernandoMillan@users.noreply.github.com>
2018-11-22 20:38:57 -0600

    Marvel's API

--- app/src/main/java/com/danylugo/bottomnavigationproyecto/MainActivity.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-
         mMain_frame = (FrameLayout) findViewById(R.id.main_frame);
         mMain_nav = (BottomNavigationView) findViewById(R.id.btmNavigationView);
 
@@ -62,12 +61,10 @@ public class MainActivity extends AppCompatActivity {
                         setFragment(villanosFragment);
                         return true;
 
-
                         default:
                             return false;
 
                 }
-                //return true;
             }
         });
     }
--- app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/SpiderSelector.java
@@ -6,10 +6,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.danylugo.bottomnavigationproyecto.MarvrelAPI.Constants;
+import com.danylugo.bottomnavigationproyecto.MarvrelAPI.RestApiAdapter;
+import com.danylugo.bottomnavigationproyecto.MarvrelAPI.Service;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class SpiderSelector extends AppCompatActivity {
 
     private CardView card;
+    private Button button;
+    private TextView tv;
+    ArrayList<Object> character;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +35,27 @@ public class SpiderSelector extends AppCompatActivity {
         getSupportActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        card = (CardView) findViewById(R.id.card);
+        card      = (CardView) findViewById(R.id.card);
+        tv        = (TextView) findViewById(R.id.tv);
+        button    = (Button) findViewById(R.id.button);
+        character = new ArrayList<>();
+
+        /*RestApiAdapter restApiAdapter = new RestApiAdapter();
+        Service service = restApiAdapter.getCharacterService();
+        retrofit2.Call<JsonObject> call = service.getDataPersonaje(Constants.APIKEY,Constants.TS,Constants.HASH);
+
+        call.enqueue(new retrofit2.Callback<JsonObject>(){
+
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });*/
     }
 
     public void onClick(View v) {
@@ -29,5 +66,4 @@ public class SpiderSelector extends AppCompatActivity {
             break;
         }
     }
-
 }
