16abd7abce246bdb9c81eaa1103e9a0ddfdbe278
Daniel Lugo <danylugo@ciencias.unam.mx>
2018-11-27 01:36:46 -0600

    Todo completo y luce bien...hasta ahora :v

--- app/src/main/java/com/danylugo/bottomnavigationproyecto/Fragments/VillanosFragment.java
+++ app/src/main/java/com/danylugo/bottomnavigationproyecto/Fragments/VillanosFragment.java
@@ -66,20 +66,37 @@ public class VillanosFragment extends Fragment {
         enemiesRecycler.setAdapter(mAdapterE);
 
         assert id != null;
-        if(!id.equals("666666")) {
+        if(!id.equals("666666") && !id.equals("1011347")) {
             ArrayList<String> ids = getSecondarysID(id);
             getDatos(ids);
         }else{
-            getDatosSpiderMoy();
+            getDatosSpiderMoy(id);
         }
 
 
         return view;
     }
 
-    private void getDatosSpiderMoy() {
-        //aqui agreguen las de spider moy :V
-        enemies.add(new Secondary("Dany Lugo","https://scontent.fmex1-1.fna.fbcdn.net/v/t1.0-9/1931526_980768155306325_8598870222290075680_n.jpg?_nc_cat=104&_nc_ht=scontent.fmex1-1.fna&oh=8a333e3b76d90d850daf8b6533873572&oe=5CA46D8B"));
+    private void getDatosSpiderMoy(String id) {
+
+        switch (id) {
+            case "1011347": //Spider-Ham
+                enemies.add(new Secondary("Doctor Octopussycat", "https://static.comicvine.com/uploads/square_small/3/33023/1433783-oct_02.jpg"));
+                enemies.add(new Secondary("Ducktor Doom", "https://static.comicvine.com/uploads/square_small/0/77/1255268-60526_3499_93521_1_peter_porker_the_sp_super.jpg"));
+                enemies.add(new Secondary("Electro", "https://static.comicvine.com/uploads/square_small/3/33023/1433777-elec_02.jpg"));
+                enemies.add(new Secondary("Flash Tombenstain Bear", "https://static.comicvine.com/uploads/square_small/13/135098/5928782-flash-bear.jpg"));
+                enemies.add(new Secondary("Green Gobbler", "https://static.comicvine.com/uploads/square_small/3/33023/1433798-gg_02.jpg"));
+                enemies.add(new Secondary("J. Jonah Jackal", "https://static.comicvine.com/uploads/square_small/13/135098/5076319-812786-jackal.jpg"));
+                enemies.add(new Secondary("Mooseterio", "https://static.comicvine.com/uploads/square_small/11/114254/2250168-mooseterio__earth_8311_.jpg"));
+                enemies.add(new Secondary("Sandmanatee", "https://static.comicvine.com/uploads/square_small/3/33023/1434233-sandmanatee_02.jpg"));
+                enemies.add(new Secondary("Pork Grind", "https://static.comicvine.com/uploads/square_small/3/33023/2478955-venom_pork_grind.jpg"));
+                enemies.add(new Secondary("The Pinhead Of Crime", "https://static.comicvine.com/uploads/square_small/10/107504/6120522-pinhead+of+crime.jpg"));
+                break;
+
+
+            default: //Spider Moy
+                enemies.add(new Secondary("Dany Lugo", "https://scontent.fmex1-1.fna.fbcdn.net/v/t1.0-9/1931526_980768155306325_8598870222290075680_n.jpg?_nc_cat=104&_nc_ht=scontent.fmex1-1.fna&oh=8a333e3b76d90d850daf8b6533873572&oe=5CA46D8B"));
+        }
     }
 
     public void getDatos(final ArrayList<String> ids){
@@ -139,15 +156,165 @@ public class VillanosFragment extends Fragment {
 
     public ArrayList<String> getSecondarysID(String id) {
         ArrayList<String> enemiesId = new ArrayList<>();
-        switch (id) {
+        switch (id){
             case "1009610":
+                enemiesId.add("1009325");
+                enemiesId.add("1009276 ");
+                enemiesId.add("1009479");
+                enemiesId.add("1009347");
+                enemiesId.add("1011128");
+                enemiesId.add("1009227");
+                enemiesId.add("1009287");
+                enemiesId.add("1009391");
+                enemiesId.add("1009404");
+                enemiesId.add("1009537");
+                enemiesId.add("1009558");
+                enemiesId.add("1009360");
+                enemiesId.add("1009464");
+                enemiesId.add("1009699");
+                enemiesId.add("1009234");
+                enemiesId.add("1011088");
+                enemiesId.add("1011426");
+                enemiesId.add("1009585");
+                enemiesId.add("1010773");
+                break;
+
+            case "1014873":
+                enemiesId.add("1009227");
+                enemiesId.add("1009281");
+                enemiesId.add("1010324");
+                enemiesId.add("1009276");
+                enemiesId.add("1010922");
+                enemiesId.add("1010931");
+                enemiesId.add("1011088");
+                enemiesId.add("1009517");
+                enemiesId.add("1011079");
+                enemiesId.add("1009314");
+                enemiesId.add("1011128");
+                enemiesId.add("1010990");
+                break;
+
+            case "1016181":
+                enemiesId.add("1009154");
+                enemiesId.add("1009185");
+                enemiesId.add("1009252");
+                enemiesId.add("1010922");
+                enemiesId.add("1010930");
+                enemiesId.add("1010842");
+                enemiesId.add("1009447");
+                enemiesId.add("1009464");
+                enemiesId.add("1009325");
+                enemiesId.add("1009507");
+                enemiesId.add("1009511");
+                enemiesId.add("1009537");
+                enemiesId.add("1009558");
+                enemiesId.add("1011079");
+                break;
+
+            case "1009608":
+                enemiesId.add("1010773");
+                enemiesId.add("1009164");
+                enemiesId.add("1010906");
+                enemiesId.add("1009203");
+                enemiesId.add("1010887");
+                enemiesId.add("1009276");
+                enemiesId.add("1009287");
+                enemiesId.add("1011420");
+                enemiesId.add("1011243");
+                enemiesId.add("1010362");
+                enemiesId.add("1009325");
+                enemiesId.add("1009571");
+                enemiesId.add("1011003");
+                enemiesId.add("1011128");
+                enemiesId.add("1009696");
+                break;
+
+            case "1011426":
+                enemiesId.add("1010906");
+                enemiesId.add("1011346");
+                enemiesId.add("1009227");
+                enemiesId.add("1009276");
+                enemiesId.add("1009287");
+                enemiesId.add("1010687");
+                enemiesId.add("1011288");
+                enemiesId.add("1009391");
+                enemiesId.add("1011088");
+                enemiesId.add("1009537");
+                enemiesId.add("1009157");
                 enemiesId.add("1009610");
-                enemiesId.add("1014873");
+                enemiesId.add("1010687");
+                enemiesId.add("1009699");
                 break;
+
+            case "1011010":
+                enemiesId.add("1014985");
+                enemiesId.add("1009507");
+                enemiesId.add("1011128");
+                enemiesId.add("1011079");
+                enemiesId.add("1009389");
+                enemiesId.add("1009464");
+                enemiesId.add("1009227");
+                enemiesId.add("1009585");
+                enemiesId.add("1009334");
+                enemiesId.add("1009675");
+                break;
+
+
+            case "1011114": //Spider-Man (Marvel Zombies)
+                enemiesId.add("1009718");
+                break;
+
+            case "1010727": //Spider-Dok (Superior Spider-Man)
+                enemiesId.add("1010371");
+                enemiesId.add("1009585");
+                enemiesId.add("1011032");
+                break;
+
+
+            case "1012295": //Spider-Man (Noir)
+                enemiesId.add("1009391");
+                enemiesId.add("1009325");
+                enemiesId.add("1009699");
+                enemiesId.add("1009234");
+                enemiesId.add("1009276");
+                break;
+
+
+            case "1009609": //Spider-Girl (May Parker)
+                enemiesId.add("1009227");
+                enemiesId.add("1009390");
+                enemiesId.add("1011247");
+                enemiesId.add("1010687 ");
+                enemiesId.add("1009322");
+                enemiesId.add("1009347");
+                enemiesId.add("1011426");
+                enemiesId.add("1011088");
+                enemiesId.add("1009325");
+                enemiesId.add("1009391");
+                break;
+
+
+            case "1011197": //Scarlet Spider (Ben Reilly)
+                enemiesId.add("1010766");
+                enemiesId.add("1009697");
+                enemiesId.add("1009227");
+                enemiesId.add("1009391");
+                enemiesId.add("1010930");
+                enemiesId.add("1011288");
+                enemiesId.add("1009380");
+                enemiesId.add("1011426");
+                enemiesId.add("1009404");
+                enemiesId.add("1011088");
+                enemiesId.add("1009464");
+                enemiesId.add("1009325");
+                enemiesId.add("1010861");
+                enemiesId.add("1010790");
+                enemiesId.add("1009566");
+                enemiesId.add("1009699");
+                break;
+
             default:
-                enemiesId.add("1016181");
-                enemiesId.add("1009608");
-                break;
+
         }
         return enemiesId;
     }
